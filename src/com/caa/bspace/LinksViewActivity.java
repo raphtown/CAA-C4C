900612002916e3bef1c9efc4cc2aee0e4023cc9a
Raphael Townshend <r@tc.com>
2012-02-04 04:02:23 -0800

    Added dynamic list view additions...  and a little surprise...

---
+++ src/com/caa/bspace/LinksViewActivity.java
@@ -0,0 +1,37 @@
+package com.caa.bspace;
+
+import java.util.ArrayList;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class LinksViewActivity extends ListActivity
+{
+	ArrayAdapter<String> adapter;
+	ArrayList<String> listItems=new ArrayList<String>();
+	
+	public void onCreate(Bundle savedInstanceState) {
+		  super.onCreate(savedInstanceState);
+
+		  listItems.add("LOL");
+		  adapter= new ArrayAdapter<String>(this, R.layout.list_item, listItems);
+		  setListAdapter(adapter);
+
+		  ListView lv = getListView();
+		  lv.setTextFilterEnabled(true);
+		  
+		  lv.setOnItemClickListener(new OnItemClickListener() {
+			    public void onItemClick(AdapterView<?> parent, View view,
+			        int position, long id) {
+			    	 listItems.add("Jay");
+			    	 adapter.notifyDataSetChanged();
+			    }
+			  });
+	}
+}
