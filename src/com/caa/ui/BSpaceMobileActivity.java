2a257b50b55b465d31a16026ca91aab04df4400f
Raphael Townshend <r@tc.com>
2012-02-04 07:46:46 -0800

    Progress bar works now!  Also better transition

--- src/com/caa/ui/BSpaceMobileActivity.java
+++ src/com/caa/ui/BSpaceMobileActivity.java
@@ -1,23 +1,46 @@
 package com.caa.ui;
 
-import com.caa.bspace.BSpaceUser;
-import com.caa.bspace.R;
-import com.caa.bspace.R.id;
-import com.caa.bspace.R.layout;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.caa.bspace.BSpaceUser;
+import com.caa.bspace.R;
 
 public class BSpaceMobileActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
 	static BSpaceUser user;
+	final Handler myHandler = new Handler() {
+		public void handleMessage(Message msg) {
+			Button submit = (Button) findViewById(R.id.loginsubmit);
+			
+			
+			ProgressBar mProgress = (ProgressBar) findViewById(R.id.loginprogress);
+			mProgress.setVisibility(ProgressBar.GONE);
+			if(user.classes.size() == 0)
+			{
+				Toast.makeText(BSpaceMobileActivity.this.getApplicationContext(), "Invalid Password", Toast.LENGTH_SHORT).show();
+			}
+			else
+			{
+
+				 Intent myIntent = new Intent(BSpaceMobileActivity.this, ClassViewActivity.class);
+			     BSpaceMobileActivity.this.startActivity(myIntent);
+
+			}
+			submit.setEnabled(true);
+			submit.setClickable(true);
+			submit.setVisibility(Button.VISIBLE);
+		}
+	};
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,33 +56,34 @@ public class BSpaceMobileActivity extends Activity implements OnClickListener {
     }
 
 	@Override
-	public void onClick(View arg0) {
-	//	
+	public void onClick(View arg0) {	
 		ProgressBar mProgress = (ProgressBar) findViewById(R.id.loginprogress);
 		
+		Button submit = (Button) findViewById(R.id.loginsubmit);
+		submit.setEnabled(false);
+		submit.setClickable(false);
+		submit.setVisibility(Button.GONE);
 		
 		// DERP this doesn't work
 		mProgress.setVisibility(ProgressBar.VISIBLE);
 		
-    	String username = ((TextView)findViewById(R.id.loginentry)).getText().toString();
-		String userpassword = ((TextView)findViewById(R.id.passwordentry)).getText().toString();
 		
-    	BSpaceMobileActivity.user = new BSpaceUser(username,
-				userpassword);
-		
-		mProgress.setVisibility(ProgressBar.GONE);
-		
+		Thread t = new Thread(new Runnable() {
 
-		if(user.classes.size() == 0)
-		{
-			Toast.makeText(this, "Invalid Password", Toast.LENGTH_SHORT).show();
-			return;
-		}
+			@Override
+			public void run() {
+				String username = ((TextView)findViewById(R.id.loginentry)).getText().toString();
+				String userpassword = ((TextView)findViewById(R.id.passwordentry)).getText().toString();
+				
+		    	BSpaceMobileActivity.user = new BSpaceUser(username,
+						userpassword);
+		    	Message msg = myHandler.obtainMessage();
+				myHandler.sendMessage(msg);
+			}
+		});
 		
-		
+		t.start();
 
-		 Intent myIntent = new Intent(BSpaceMobileActivity.this, ClassViewActivity.class);
-	     BSpaceMobileActivity.this.startActivity(myIntent);
 		
 	}
 }
