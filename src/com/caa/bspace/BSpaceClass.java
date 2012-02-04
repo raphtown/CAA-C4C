0b33cc6fcc0c874b03c4828ad57f85ffef9e6f7b
Kenny Do <kennydo@berkeley.edu>
2012-02-04 09:07:21 -0800

    files tab should work

--- src/com/caa/bspace/BSpaceClass.java
+++ src/com/caa/bspace/BSpaceClass.java
@@ -1,30 +1,25 @@
 package com.caa.bspace;
 
-import java.lang.reflect.Constructor;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.htmlcleaner.*;
-
-import android.text.Html;
 
 public class BSpaceClass {
     
     private static final String BSPACE_CLASS_BASE_URL = "https://bspace.berkeley.edu/portal/site/";
-    private static final String XPATH_SIDEBAR = "//div[@id='toolMenu']//a";
+    private static final String XPATH_SIDEBAR = "//div[@id='toolMenu']//span";
     
-    private static final String[] supportedResources = { "Syllabus", "Resources", "Gradebook" };
+    private static final String[] supportedTools = { "Syllabus", "Resources", "Gradebook" };
     
     private BSpaceUser user;
     public String uuid, name;
-    private LinkedList<BSpaceResource> resources;
+    private LinkedList<String> tools;
     
     public BSpaceClass(BSpaceUser user, String uuid, String name) {
         this.user = user;
         this.uuid = uuid;
-        this.name = Html.fromHtml(name).toString();
-        this.resources = new LinkedList<BSpaceResource>();
+        this.name = name;
+        this.tools = new LinkedList<String>();
     }
     
     public void parseSidebar() {
@@ -36,44 +31,15 @@ public class BSpaceClass {
         
         try {
             Object[] sidebarEls = root.evaluateXPath(XPATH_SIDEBAR);
-            HashSet<String> supported = new HashSet<String>(Arrays.asList(supportedResources));
+            HashSet<String> supported = new HashSet<String>(Arrays.asList(supportedTools));
             HashSet<String> actual = new HashSet<String>();
-            HashMap<String, String> uuidMap = new HashMap<String, String>();
             for (Object obj : sidebarEls) {
-                TagNode el = (TagNode) obj;
-                String[] components = el.getAttributeByName("href").split("/");
-                try {
-                    UUID uuidTest = UUID.fromString(components[components.length - 1]);
-                    String name = ((TagNode) el.evaluateXPath("span")[0]).getText().toString();
-                    String uuid = components[components.length - 1];
-                    actual.add(name);
-                    uuidMap.put(name, uuid);
-                    
-                } catch (Exception e) {
-                    // not a valid UUID; do nothing
-                }
+                actual.add(((TagNode) obj).getText().toString());
             }
-            supported.retainAll(actual); // Intersect support and actual resource list; result stored in supported
-            for (String resourceName : supported) {
-                // Sigh...
-                BSpaceResource rsrc;
-                String uuid = uuidMap.get(resourceName);
-                if (resourceName.equals("Syllabus")) {
-                    rsrc = new BSpaceSyllabusResource(this, uuid);
-                } else if (resourceName.equals("Resources")) {
-                    rsrc = new BSpaceFilesResource(this, uuid);
-                    ((BSpaceFilesResource) rsrc).initUser(user);
-                } else if (resourceName.equals("Gradebook")) {
-                    rsrc = new BSpaceGradebookResource(this, uuid);
-                } else {
-                    rsrc = null; // -__-
-                }
-//                String className = "BSpace" + Pattern.compile("[^\\w]").matcher(resourceName).replaceAll("") + "Resource";
-//                BSpaceResource rsrc = new BSpaceSyllabusResource(this, uuidMap.get(resourceName));
-//                BSpaceResource rsrc = (BSpaceResource) Class.forName(className).getConstructor(BSpaceClass.class, String.class).newInstance(this, uuidMap.get(resourceName));
-                resources.add(rsrc);
-            }
+            supported.retainAll(actual); // Intersect support and actual tool list; result stored in supported
+            tools.addAll(supported);
         } catch (Exception e) {
+            System.out.println("I think I will print a stacktrace.");
             e.printStackTrace();
         }
     }
--- src/com/caa/bspace/BSpaceFilesResource.java
+++ src/com/caa/bspace/BSpaceFilesResource.java
@@ -2,119 +2,15 @@ package com.caa.bspace;
 
 import java.util.ArrayList;
 
-import org.apache.http.HttpConnection;
-import org.htmlcleaner.HtmlCleaner;
-import org.htmlcleaner.TagNode;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
 import android.net.Uri;
 import android.util.Log;
 
-public class BSpaceFilesResource {
-	
-	private BSpaceClass bspaceClass;
-	private BSpaceUser user;
-	
-	public BSpaceDirectory rootDirectory;
-	
-	public interface BSpaceFilesItem{
-	}
-	
-	public class BSpaceFile implements BSpaceFilesItem{
-		private BSpaceDirectory parentDirectory;
-		private String name;
-		
-		public BSpaceFile(BSpaceDirectory parentDirectory, String name){
-			this.parentDirectory = parentDirectory;
-			this.name = name;
-		}
-		
-		public String toString(){
-			return name;
-		}
-	}
-	
-	public class BSpaceDirectory implements BSpaceFilesItem{
-		public ArrayList<BSpaceDirectory> subdirectories;
-		public ArrayList<BSpaceFile> files;
-		
-		private BSpaceDirectory parentDirectory;
-		private String name;
-		private String url;
-		
-		public BSpaceDirectory(){
-			url = "https://bspace.berkeley.edu/dav/" + bspaceClass.uuid;
-		}
-		
-		public String toString(){
-			return name;
-		}
-		
-		public BSpaceDirectory(BSpaceDirectory parentDirectory, String name){
-			this.parentDirectory = parentDirectory;
-			this.name = name;
-			url = parentDirectory.url + "/" + Uri.encode(this.name);
-			
-			
-		}
-		
-		public void getItems(){
-			String html = user.openPageWithAuth(url);
-			
-			subdirectories = new ArrayList<BSpaceDirectory>();
-			files = new ArrayList<BSpaceFile>();
-			
-			int tableStartIndex = html.indexOf("<table>") + "<table>".length();
-			int tableEndIndex = html.indexOf("</table>", tableStartIndex) - "</table>".length();
-			if(tableStartIndex == -1 || tableEndIndex == -1){
-				Log.d("bspaceFiles", html);
-				Log.w("bspacefiles", "unable to parse table");
-				return;
-			}
-			String tableHtml = html.substring(tableStartIndex, tableEndIndex);
-			
-			
-			
-			for(String tableRow : tableHtml.split("</tr>")){
-				String nameSegment = tableRow.split("</a>")[0];
-				String name = nameSegment.substring(nameSegment.lastIndexOf("\">") + 2);
-				
-				if (name == "Up one level"){
-					continue;
-				}
-				if(subdirectories == null){
-					Log.e("bspacefiles", "SUBDIRECTORIES IS NULL");
-				}
-				if(files == null){
-					Log.e("bspacefiles", "FILES IS NULL");
-				}
-				if(tableRow.contains("<b>Folder</b>")){
-					Log.d("bspacefile", "Adding directory " + name);
-					subdirectories.add(new BSpaceDirectory(this, name));
-				} else {
-					Log.d("bspacefile", "Adding file " + name);
-					files.add(new BSpaceFile(this, name));
-				}
-			}
-		}
-	}
-	
-	public BSpaceFilesResource(BSpaceUser user, BSpaceClass bspaceClass){
-		this.bspaceClass = bspaceClass;
-		this.user = user;
-		
-		rootDirectory = new BSpaceDirectory();
-		rootDirectory.getItems();
-	}
-=======
 public class BSpaceFilesResource implements BSpaceResource {
 
     private BSpaceClass ownerClass;
-    private String classUid;
     private BSpaceUser user;
 
-    private BSpaceDirectory rootDirectory;
+    public BSpaceDirectory rootDirectory;
 
     public interface BSpaceFilesItem{
 	}
@@ -146,7 +42,7 @@ public class BSpaceFilesResource implements BSpaceResource {
 		}
         
         public BSpaceDirectory() {
-            url = "https://bspace.berkeley.edu/dav/" + classUid;
+            url = "https://bspace.berkeley.edu/dav/" + ownerClass.uuid;
         }
 
         public BSpaceDirectory(BSpaceDirectory parentDirectory, String name) {
@@ -159,13 +55,18 @@ public class BSpaceFilesResource implements BSpaceResource {
         public void getItems() {
             String html = user.openPageWithAuth(url);
 
+            subdirectories = new ArrayList<BSpaceDirectory>();
+            files = new ArrayList<BSpaceFile>();
+            
             int tableStartIndex = html.indexOf("<table>") + "<table>".length();
             int tableEndIndex = html.indexOf("</table>", tableStartIndex) - "</table>".length();
 
+            if(tableStartIndex == -1 || tableEndIndex == -1){
+            	Log.w("bspacefiles", "Unable to parse table");
+            	return;
+            }
+            
             String tableHtml = html.substring(tableStartIndex, tableEndIndex);
-
-            subdirectories = new ArrayList<BSpaceDirectory>();
-            files = new ArrayList<BSpaceFile>();
 
             for (String tableRow : tableHtml.split("</tr>")) {
                 String nameSegment = tableRow.split("</a>")[0];
@@ -191,9 +92,9 @@ public class BSpaceFilesResource implements BSpaceResource {
         }
     }
 
-    public BSpaceFilesResource(BSpaceClass ownerClass, String classUid) {
-        this.ownerClass = ownerClass;
-        this.classUid = classUid;
+    public BSpaceFilesResource(BSpaceUser user, BSpaceClass bspaceClass) {
+        this.user = user;
+        this.ownerClass = bspaceClass;
 
         rootDirectory = new BSpaceDirectory();
         rootDirectory.getItems();
